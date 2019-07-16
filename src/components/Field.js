df131b7c3558d7523ffebd65d42cd500afc3a4a5
annettesunday <annette.odhiambo@andela.com>
2019-07-16 13:09:38 +0300

    ch(components): consuming the context value

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,10 +1,13 @@
 import React from "react";
+import LanguageContext from "../contexts/LanguangeContext";
 
 class Field extends React.Component {
+  static contextType = LanguageContext;
   render() {
+    const text = this.context === "english" ? "Name" : "Naam";
     return (
       <div className="ui field">
-        <label>Name</label>
+        <label>{text}</label>
         <input />
       </div>
     );
