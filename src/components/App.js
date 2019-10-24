e462cf4cd6ca5fcf812e0a3a31e6635a1d3c6b15
annettesunday <annette.odhiambo@andela.com>
2019-10-24 18:04:03 +0300

    ch(LnaguageSelector): scope out LanguageSelector component

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import UserCreate from "./UserCreate";
 import LanguangeContext from "../contexts/LanguangeContext";
 import ColorContext from "../contexts/ColorContext";
+import LanguageSelector from "./LanguageSelector";
 
 class App extends React.Component {
   state = {
-    language: "English"
+    language: "english"
   };
 
   onLanguageChange = language => {
@@ -16,17 +17,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a language:
-          <i
-            className="flag us"
-            onClick={() => this.onLanguageChange("english")}
-          />
-          <i
-            className="flag nl"
-            onClick={() => this.onLanguageChange("dutch")}
-          />
-        </div>
+        <LanguageSelector onLanguageChange={this.onLanguageChange} />
         <LanguangeContext.Provider value={this.state.language}>
           <ColorContext.Provider value="red">
             <UserCreate />
